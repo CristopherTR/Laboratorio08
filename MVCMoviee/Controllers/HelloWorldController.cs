52280c66896d827fa0078c52947c8dafa1943e89
CristopherTR <cristopher.torres@tecsup.edu.pe>
2019-10-07 18:44:34 -0500

    version 2

---
+++ MVCMoviee/Controllers/HelloWorldController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMoviee.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+
+        /*public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }*/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome (string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello" + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+        /*public string Welcome(string name, int numTimes = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello" + name + ", ID:");
+            return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
+            return "This is the welcome action method...";
+        }*/
+    }
+}
---
+++ MVCMoviee/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCMoviee.MvcApplication" Language="C#" %>
